873b383f4fded38044f925642f5a319cec055aa8
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-23 23:23:12 +0200

    feat: add mutation for creating albums and integrate with API

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
 
 const albumsApi = createApi({
   reducers: 'albums',
@@ -7,6 +8,15 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      addAlbum: builder.mutation({
+        query: (user) => {
+          return {
+            url: '/albums',
+            method: 'POST',
+            body: { userId: user.id, title: faker.commerce.productName() },
+          };
+        },
+      }),
       fetchAlbums: builder.query({
         query: (user) => {
           return {
@@ -20,5 +30,5 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
