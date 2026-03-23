b442478d175d695b7ea8f3add9ca1c93b6829397
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-24 00:05:51 +0200

    refactor: replace `isLoading` with `isFetching` in AlbumsList for consistency

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -4,7 +4,7 @@ import Button from './Button';
 import AlbumsListItem from './AlbumsListItem';
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
@@ -12,7 +12,7 @@ function AlbumsList({ user }) {
   };
 
   let content;
-  if (isLoading) {
+  if (isFetching) {
     content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
@@ -26,7 +26,7 @@ function AlbumsList({ user }) {
     <div>
       <div className="m-2 flex flex-row items-center justify-between">
         <h3 className="text-lg font-bold">Albums for {user.name}</h3>
-        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+        <Button loading={results.isFetching} onClick={handleAddAlbum}>
           + Add Album
         </Button>
       </div>
