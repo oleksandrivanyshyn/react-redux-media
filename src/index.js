7388d0bea22e8eaf7fca57749dd45b33acb1af61
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-23 21:54:02 +0200

    feat: implement UsersList and UsersListItem with Redux integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './index.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import App from './App';
+
 const el = document.getElementById('root');
 const root = createRoot(el);
 
