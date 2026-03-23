e71b8811c3aba34203e7f95eb6450e7bb44248f6
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-23 23:56:18 +0200

    feat: add "Remove Album" mutation to albums API and integrate with Redux store

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -8,6 +8,14 @@ const albumsApi = createApi({
   }),
   endpoints(builder) {
     return {
+      removeAlbum: builder.mutation({
+        query: (album) => {
+          return {
+            url: `/albums/${album.id}`,
+            method: 'DELETE',
+          };
+        },
+      }),
       addAlbum: builder.mutation({
         invalidatesTags: (result, error, user) => {
           return [{ type: 'Album', id: user.id }];
@@ -36,5 +44,9 @@ const albumsApi = createApi({
   },
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} = albumsApi;
 export { albumsApi };
