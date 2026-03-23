d8e01c6b5a34b59cdfb567b276ef71fca7682c28
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-23 22:25:12 +0200

    feat: integrate albums API with Redux store and add query for fetching albums

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi';
+import { setupListeners } from '@reduxjs/toolkit/src/query';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
-
+setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
+export { useFetchAlbumsQuery } from './apis/albumsApi';
